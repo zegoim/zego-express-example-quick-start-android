96dbe262c0976cad148920de69d036f940007c0a
cjjchen <celadonbird@sina.com>
2019-11-19 18:11:47 +0800

    new

---
+++ src/ZegoExpressExample/common/src/main/java/com/zego/common/GetAppIdConfig.java
@@ -0,0 +1,15 @@
+package com.zego.common;
+
+public class GetAppIdConfig {
+    /** Please fill appID  and appSign */
+
+    /** Please register from ZEGO management console, https://console-express.zego.im/acount/register，
+     *  The format is 123456789L
+     */
+    public static final long appId = 1739272706L;
+
+    /** 64 characters，Please register from ZEGO management console, https://console-express.zego.im/acount/register，
+     *  The format is "0123456789012345678901234567890123456789012345678901234567890123"
+     */
+    public static final String appSign = "1ec3f85cb2f21370264eb371c8c65ca37fa33b9defef2a85e0c899ae82c0f6f8";
+}
