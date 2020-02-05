12cea62ebf30952ad781404bfba1a022249b39df
hsx2117 <hsx2117@icloud.com>
2020-02-05 19:09:39 +0800

    add: 增加媒体播放器示例专题

---
+++ src/ZegoExpressExample/common/src/main/java/im/zego/common/GetAppIDConfig.java
@@ -0,0 +1,15 @@
+package im.zego.common;
+
+public class GetAppIDConfig {
+    /** Please fill appID  and appSign */
+
+    /** Please register from ZEGO management console, https://console-express.zego.im/acount/register，
+     *  The format is 123456789L
+     */
+    public static final long appID = ;
+
+    /** 64 characters，Please register from ZEGO management console, https://console-express.zego.im/acount/register，
+     *  The format is "0123456789012345678901234567890123456789012345678901234567890123"
+     */
+    public static final String appSign = ;
+}
